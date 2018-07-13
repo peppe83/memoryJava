f8e49105d3dbc34941ec29f069962cb53da4a9c2
peppe83 <8167474+peppe83@users.noreply.github.com>
2018-07-13 11:58:04 +0200

    prova

--- src/test/java/secure/Decrypt.java
+++ src/test/java/secure/Decrypt.java
@@ -48,7 +48,7 @@ public class Decrypt {
 //            String str = new String(decodedData);
 //            System.out.println("DECODED FILE CONTENTS : " + str);
         	
-        	
+        	//prova
         	byte[] fileData = readFile();
         	KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
             keyGenerator.init(128);
