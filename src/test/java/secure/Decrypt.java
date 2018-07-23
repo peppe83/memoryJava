c03dc11344fa20f4a70ed9ee048ab5cfbbbe29b5
vimo <8167474+peppe83@users.noreply.github.com>
2018-07-23 21:43:57 +0200

    agg

--- src/test/java/secure/Decrypt.java
+++ src/test/java/secure/Decrypt.java
@@ -5,45 +5,48 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Decrypt {
+	//static String encryptedFileName = "latlng.mem";
+	static String encryptedFileName = "latitude.mem";
 	static String myPwdEncode = "giuseppegiuseppe";
-	static String encryptedFileNameLog = "latlng.mem";
-	static String encryptedFileNameAcc = "latitude.mem";
 	
 	public static void main(String[] args) {
 		try {
 			
-			File file = new File("e:" + File.separator + "DevApps" + File.separator + "memory" + File.separator +encryptedFileNameLog);
-			decodeFile(file);
-	 
+            SecretKeySpec yourKey = new SecretKeySpec(myPwdEncode.getBytes(), "AES");
+            byte[] decodedData = decodeFile(yourKey, readFile());
+            String str = new String(decodedData);
+            System.out.println("DECODED FILE CONTENTS: <<" + str + ">>");
+		} catch( NoSuchAlgorithmException | InvalidKeySpecException e ) {
+			throw new RuntimeException( e );
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static String decodeFile(File file) {
-        if (file==null || !file.exists()) return null;
-        try {
-            SecretKeySpec yourKey = new SecretKeySpec(myPwdEncode.getBytes(), "AES");
-            byte[] decodedData = decodeFile(yourKey, readFile(file));
-            String str = new String(decodedData);
-            System.out.println("DECODED FILE CONTENTS: ");
-            System.out.println(str);
-            return str;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public static byte[] decodeFile(SecretKey yourKey, byte[] fileData) throws Exception {
+    	byte[] data = yourKey.getEncoded();
+//        SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES/ECB/PKCS5Padding");
+    	SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
 
-	public static byte[] readFile(File file) {
+        byte[] decrypted = cipher.doFinal(fileData);
+        return decrypted;
+    }
+    
+    public static byte[] readFile() {
         byte[] contents = null;
+
+        File file = new File("e:" + File.separator + "DevApps" + File.separator + "memory" + File.separator + encryptedFileName);
 
         //File file = new File(Environment.getExternalStorageDirectory() + File.separator, encryptedFileName);
         int size = (int) file.length();
@@ -61,18 +64,5 @@ public class Decrypt {
         }
         return contents;
     }
-    
-    public static byte[] decodeFile(SecretKey yourKey, byte[] fileData) throws Exception {
-        byte[] data = yourKey.getEncoded();
-//        SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES/ECB/PKCS5Padding");
-//        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-
-        byte[] decrypted = cipher.doFinal(fileData);
-        return decrypted;
-    }
-
 
 }
