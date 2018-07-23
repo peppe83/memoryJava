451a5b85a1220b9c99d733d6ffa81947400fb637
peppe83 <8167474+peppe83@users.noreply.github.com>
2018-07-23 17:29:38 +0200

    ok

--- src/test/java/secure/Decrypt.java
+++ src/test/java/secure/Decrypt.java
@@ -5,80 +5,45 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Security;
-import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Decrypt {
-
+	static String myPwdEncode = "giuseppegiuseppe";
+	static String encryptedFileNameLog = "latlng.mem";
+	static String encryptedFileNameAcc = "latitude.mem";
+	
 	public static void main(String[] args) {
 		try {
 			
-			decodeFile();
+			File file = new File("e:" + File.separator + "DevApps" + File.separator + "memory" + File.separator +encryptedFileNameLog);
+			decodeFile(file);
 	 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 	
-	public static void decodeFile() {
+	public static String decodeFile(File file) {
+        if (file==null || !file.exists()) return null;
         try {
-//            String myPwdEncode = "giuseppegiuseppe";
-//            SecretKeySpec yourKey = new SecretKeySpec(myPwdEncode.getBytes(), "AES");
-//            byte[] fileData = readFile();
-//            
-//            byte[] data = yourKey.getEncoded();
-//            SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES/ECB/PKCS5Padding");
-//            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-//            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-//
-//            byte[] decodedData = cipher.doFinal(fileData);
-//            
-//
-//            String str = new String(decodedData);
-//            System.out.println("DECODED FILE CONTENTS : " + str);
-        	
-        	//prova1
-        	byte[] fileData = readFile();
-        	KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            keyGenerator.init(128);
-            SecretKey key = keyGenerator.generateKey();
-            
-//        	IvParameterSpec iv = new IvParameterSpec("0102030405060708".getBytes());
-//            SecretKeySpec spec = new SecretKeySpec(key.getEncoded(), "AES");
-//            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-//            cipher.init(Cipher.DECRYPT_MODE, spec, iv);
-            
-            
-            SecretKeySpec spec = new SecretKeySpec(key.getEncoded(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");//AES/ECB/PKCS5Padding		AES/CBC/PKCS5Padding
-            cipher.init(Cipher.DECRYPT_MODE, spec);
-
-            byte[] decodedData = cipher.doFinal(fileData);
+            SecretKeySpec yourKey = new SecretKeySpec(myPwdEncode.getBytes(), "AES");
+            byte[] decodedData = decodeFile(yourKey, readFile(file));
             String str = new String(decodedData);
-            System.out.println("DECODED FILE CONTENTS : " + str);
-        	
-        	
+            System.out.println("DECODED FILE CONTENTS: ");
+            System.out.println(str);
+            return str;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-	public static byte[] readFile() {
+	public static byte[] readFile(File file) {
         byte[] contents = null;
-
-        String encryptedFileName = "memory.mem";
-        File file = new File("c:" + File.separator +encryptedFileName);
 
         //File file = new File(Environment.getExternalStorageDirectory() + File.separator, encryptedFileName);
         int size = (int) file.length();
@@ -97,6 +62,17 @@ public class Decrypt {
         return contents;
     }
     
+    public static byte[] decodeFile(SecretKey yourKey, byte[] fileData) throws Exception {
+        byte[] data = yourKey.getEncoded();
+//        SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES/ECB/PKCS5Padding");
+//        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        SecretKeySpec skeySpec = new SecretKeySpec(data, 0, data.length, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+
+        byte[] decrypted = cipher.doFinal(fileData);
+        return decrypted;
+    }
 
 
 }
